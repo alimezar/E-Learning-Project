72cc784a5b0f212c73815b36648b8e31edcbe714
VizardeX <wajeehf168@gmail.com>
2024-12-03 21:04:07 +0200

    Update quizzes.schema.ts

--- backend/src/quizzes/quizzes.schema.ts
+++ backend/src/quizzes/quizzes.schema.ts
@@ -8,8 +8,30 @@ export class Quizzes {
   @Prop({ type: Types.ObjectId, ref: 'Module', required: true }) 
   moduleId: Types.ObjectId;
 
-  @Prop({ type: [Object], required: true })
-  questions: Record<string, any>[];
+  @Prop({
+    type: [
+      {
+        question: { type: String, required: true },
+        difficulty: { type: String, required: true },
+        options: { type: [String], required: true},
+        answer: { type: String, required: true },
+        moduleId: { 
+          type: Types.ObjectId, 
+          ref: 'Module',  // Reference to the Module schema
+          required: true 
+        },
+      },
+    ],
+    required: true,
+  })
+  questions: {
+    question: string;
+    difficulty: string;
+    options: string[];
+    answer: string;
+    moduleId: Types.ObjectId; // has the id of the module
+  }[];
+  
 }
 
 export const QuizSchema = SchemaFactory.createForClass(Quizzes);
