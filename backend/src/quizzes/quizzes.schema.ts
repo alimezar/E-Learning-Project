46513488969fa9fc5ee78ef99790950b32257f65
VizardeX <wajeehf168@gmail.com>
2024-12-04 18:43:21 +0200

    returned to M1 schema

--- backend/src/quizzes/quizzes.schema.ts
+++ backend/src/quizzes/quizzes.schema.ts
@@ -8,29 +8,8 @@ export class Quizzes {
   @Prop({ type: Types.ObjectId, ref: 'Module', required: true }) 
   moduleId: Types.ObjectId;
 
-  @Prop({
-    type: [
-      {
-        question: { type: String, required: true },
-        difficulty: { type: String, required: true },
-        options: { type: [String], required: true},
-        answer: { type: String, required: true },
-        moduleId: { 
-          type: Types.ObjectId, 
-          ref: 'Module',  // Reference to the Module schema
-          required: true 
-        },
-      },
-    ],
-    required: true,
-  })
-  questions: {
-    question: string;
-    difficulty: string;
-    options: string[];
-    answer: string;
-    moduleId: Types.ObjectId; // has the id of the module
-  }[];
+  @Prop({ type: [Object], required: true })
+  questions: Record<string, any>[];
   
 }
 
