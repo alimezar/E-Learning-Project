9a5020f8d163b0286437e4fa7449c9e88b536a39
Ali Sherif <ali.mezar@gmail.com>
2024-12-08 17:16:43 +0200

    Fixes

--- backend/src/Forums/reply/reply.service.ts
+++ backend/src/Forums/reply/reply.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Reply } from './reply.schema';
-import { NotificationService } from '../../Notification/notification.service'; 
+import { NotificationService } from '../../notifications/notifications.service'; 
 
 @Injectable()
 export class ReplyService {
