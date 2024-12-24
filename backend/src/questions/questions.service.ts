057605fa55558c804d9087556bc9496b307c7993
VizardeX <wajeehf168@gmail.com>
2024-12-24 17:11:17 +0200

    update q by instructor

--- backend/src/questions/questions.service.ts
+++ backend/src/questions/questions.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
+import { Injectable, BadRequestException, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Questions, QuestionDocument } from './questions.schema';
@@ -35,10 +35,18 @@ export class QuestionsService {
 
   // Update a question by ID
   async updateQuestion(id: string, updateData: Partial<Questions>): Promise<Questions> {
+    const user = await this.userModel.findById(updateData.userId);
+    if (!user || user.role !== "instructor") {
+      throw new BadRequestException("Only an instructor can update a question");
+    }
+
     const updatedQuestion = await this.questionModel
       .findByIdAndUpdate(id, updateData, { new: true })
       .exec();
-    return updatedQuestion;  
+    if (!updatedQuestion) {
+      throw new NotFoundException(`Question with ID "${id}" not found.`);
+    }
+    return updatedQuestion;
   }
 
   // Delete a question by ID
